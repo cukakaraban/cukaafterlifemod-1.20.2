a12288bdd370413ad4e3273318e27167451b7e00
cukakaraban <ultimatejonysnake@gmail.com>
2023-10-12 17:21:31 +0300

    something

---
+++ src/main/java/com/example/afterlife/CukaAfterLifeModClient.java
@@ -0,0 +1,10 @@
+package com.example.afterlife;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CukaAfterLifeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
